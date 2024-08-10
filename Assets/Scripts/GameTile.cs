454875933e6e63945fed16e3814e8d4b66ad5863
LeeJonesDev <leeastonjones.development@gmail.com>
2024-08-10 16:12:10 -0500

    Starting to add more structure for Life in a unity structure

--- Assets/Scripts/GameTile.cs
+++ Assets/Scripts/GameTile.cs
@@ -2,30 +2,36 @@ using UnityEngine;
 
 public class GameTile : MonoBehaviour
 {
-    public bool isAlive = true;
+    public bool isAlive;
+    public bool shouldLive;
 
     public CartesianCoordinates Coordinates;
 
-    public CartesianCoordinates LeftNeighbor;
-    public CartesianCoordinates RightNeighbor;
-    public CartesianCoordinates AboveNeighbor;
-    public CartesianCoordinates BelowNeighbor;
+    public CartesianCoordinates LeftNeighborCoords;
+    public GameTile LeftNeighborTile;
 
-    public void DetermineLifecycle()
+    public CartesianCoordinates RightNeighborCoords;
+    public GameTile RightNeighborTile;
+
+    public CartesianCoordinates AboveNeighborCoords;
+    public GameTile AboveNeighborTile;
+
+    public CartesianCoordinates BelowNeighborCoords;
+    public GameTile BelowNeighborTile;
+
+    public bool DetermineIfShouldLive()
     {
-        //TODO: determine lifecycle
-        //Set lifecycle
-        if (TryGetComponent<SpriteRenderer>(out var spriteRenderer))
-        {
-            if (!isAlive)
-            {
-                spriteRenderer.color = Color.white;
-            }
-            else
-            {
-                spriteRenderer.color = Color.black;
-            }
-        }
+        /*
+            1. Any live cell with fewer than two live neighbors dies, as if by underpopulation.
 
+            2. Any live cell with two or three live neighbors lives on to the next generation.
+
+            3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+
+            4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+        */
+
+        //if(isAlive && )
+        return true;
     }
 }
